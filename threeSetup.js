4b31556c26c8bff19db0eb49020308fee7d43ceb
christophthonhauser <c.thonhauser97@gmail.com>
2024-09-18 18:42:38 +0200

    add random scene

--- threeSetup.js
+++ threeSetup.js
@@ -1,4 +1,9 @@
 import * as THREE from 'three';
+import {TextureLoader} from "three";
+
+const GRASS_GREEN = 0x55e56f;
+
+const CREAM = 0xfffdd0;
 
 /**
  * Sets up a basic Three.js scene with a rotating cube.
@@ -8,23 +13,34 @@ import * as THREE from 'three';
  */
 export function setupThreeJS() {
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color(CREAM);
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const renderer = new THREE.WebGLRenderer();
+    const renderer = new THREE.WebGLRenderer()
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setAnimationLoop(animate);
     document.body.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    const geometry = new THREE.BoxGeometry(1, 2, 1);
+    const material = new THREE.MeshBasicMaterial({color: GRASS_GREEN});
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
-    camera.position.z = 5;
+    const floorGeometry = new THREE.PlaneGeometry(10, 10);
+    const textureLoader = new TextureLoader();
+    const floorTexture = textureLoader.load('https://threejs.org/examples/textures/terrain/grasslight-big.jpg');
+    const floorMaterial = new THREE.MeshBasicMaterial({map: floorTexture});
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.rotation.x = -Math.PI / 2;
+    floor.position.y = -2;
+    scene.add(floor);
+
+    camera.position.y = 1;
+    camera.position.z = 10;
 
     function animate() {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        cube.rotation.x += 0.05;
+        cube.rotation.y += 0.05;
         renderer.render(scene, camera);
     }
 }
